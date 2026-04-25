90723a28d79942d537b0c2fc8942bac7880ee88e
vasugit-0457 <vasusharma201213@gmail.com>
2026-04-25 09:55:56 +0530

    Delete router.js

--- router.js
+++
@@ -1,8 +0,0 @@
-// js/router.js
-
-export function showScreen(id) {
-    document.querySelectorAll('.screen').forEach(s => s.classList.remove('active'));
-    const target = document.getElementById(id);
-    if(target) target.classList.add('active');
-    window.scrollTo(0, 0);
-}
